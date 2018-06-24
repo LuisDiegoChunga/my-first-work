271a81f974cb2280f5b18544420a2546fe528982
LuisDiegoChunga <luisdiegochunga@hotmail.com>
2018-06-24 10:19:57 -0500

    Cambio realizado 24 de junio del 2018 a las 10:19 a.m

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.ingresar_variables),
-    url(r'^resultado/(?P<result>[0-9]+)/$', views.resultado,  name='resultado')
+    url(r'^resultado/(?P<pk>[0-9]+)/$', views.resultado,  name='resultado')
 ]
